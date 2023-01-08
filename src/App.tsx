ed7c30478d115566c75f2e44d81c65f4a129a65e
unknown <elizvood@gmail.com>
2023-01-08 18:34:31 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div className={app-wrapper}>
+          <header className={header}>
+              <img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fru.wikipedia.org%2Fwiki%2F%25D0%25A4%25D0%25B0%25D0%25B9%25D0%25BB%3AFC_Wolverhampton_Wanderers_Logo.svg&psig=AOvVaw0I3Ts9CU7GHHpiDoWE727E&ust=1673179375084000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCPiq6oS1tfwCFQAAAAAdAAAAABAI' />
+          </header>
+          <nav className={nav}>
+              <div>
+                  Profile
+              </div>
+              <div>Messeges</div>
+          </nav>
+ <div className={content}>
+     Main content
+ </div>
+          </div>
+  );
+}
+export default App;
